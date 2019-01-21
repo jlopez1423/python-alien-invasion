e6c3c396df68b4c54b60f4430501cc52486cfe8b
Jose <jalopez1423@gmail.com>
2019-01-20 20:12:28 -0800

    added bullets front end

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,6 @@
 import pygame
 import game_functions as gf
-
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 from dobie import Dobie
@@ -16,13 +16,16 @@ def run_game():
     # Make a ship
     # ship = Ship(screen)
     dobie = Dobie(ai_settings, screen)
+    # Make a group to store bullets in.
+    bullets = Group()
 
     # Start the main loop for the game.
     while True:
         # Watch for keyboard and mouse events.
-        gf.check_events(dobie)
+        gf.check_events(ai_settings, screen, dobie, bullets)
         dobie.update()
-        gf.update_screen(ai_settings, screen, dobie)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, dobie, bullets)
 
 
 run_game()
