c7578cf3aa4012561786bb491cb8d93034b8bca3
Jose <jalopez1423@gmail.com>
2019-03-07 20:07:34 -0800

    Adding button class

--- game_stats.py
+++ game_stats.py
@@ -7,6 +7,9 @@ class GameStats:
         # Start alien invasion in an active state.
         self.game_active = True
 
+        # Start game in an inactive state.
+        self.game_active = False
+
     def reset_stats(self):
         """Initialize stats that can change during the game"""
         self.ships_left = self.ai_settings.ship_limit
